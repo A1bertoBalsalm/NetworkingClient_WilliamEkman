1c08b740c9a27063b4026a6b0059b86070cec9ee
william <ekman.william2005@gmail.com>
2024-05-28 14:11:30 +0200

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
-import java.net.InetAddress;
-import java.net.Socket;
+
+import java.net.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -16,46 +16,51 @@ public class Main {
         }
 
         for (int i=0; i <10; i++){
+            try{
+                // Create a client socket
+                client = new Socket(InetAddress.getLocalHost(),portnumber);
+                System.out.println("Client socket is created "+client);
 
-            // Create a client socket
-            client = new Socket(InetAddress.getLocalHost(),portnumber);
-            System.out.println("Client socket is created "+client);
+                // Create output
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut, true);
 
-            // Create output
-            OutputStream clientOut = client.getOutputStream();
-            PrintWriter pw = new PrintWriter(clientOut, true);
+                // Create Input
 
-            // Create Input
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
 
-            InputStream clientIn = client.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                // Create standard input
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-            // Create standard input
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Enter your name. Type Bye to exit.");
 
-            System.out.println("Enter your name. Type Bye to exit.");
+                // Read data from standerd input and write it
 
-            // Read data from standerd input and write it
+                var msg = stdIn.readLine().trim();
+                pw.println(msg);
 
-            msg = stdIn.readLine().trim();
-            pw.println(msg);
+                // Read data from client socket
+                System.out.println("Message retunred from the server = "+br.readLine());
 
-            // Read data from client socket
-            System.out.println("Message retunred from the server = "+br.readLine());
+                pw.close();
+                br.close();
+                client.close();
 
-            pw.close();
-            br.close();
-            client.close();
+                // Stop
+                if(msg.equalsIgnoreCase("Bye")){
+                    break;
+                }
 
-            // Stop
-            if(msg.equalsIgnoreCase("Bye")){
-                break;
+
+            } catch (IOException ie){
+                System.out.println("Error: "+ie);
             }
 
-
-        } catch (IOException ie){
-            System.out.println("Error: "+ie);
         }
+
+
+
 
 
 
